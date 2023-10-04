509fc4d2bfddd0ace618fae281ee16c4a155d66f
k22002066@kcl.ac.uk <akshat.singh@kcl.ac.uk>
2023-10-04 15:32:02 +0100

    Task completed

--- median.py
+++ median.py
@@ -8,5 +8,13 @@ while True:
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
+        numbers.sort()
         break
-print(numbers)
+n = len(numbers)
+if n%2 == 0:
+    middleA = numbers[(n//2) -1]
+    middleB = numbers[n//2]  
+    median = (middleA + middleB) / 2         
+else:
+    median = numbers[n//2]        
+print("Median :" , median)
